e6a40ffa545ef293cd11937c5ad12db976bbb01b
John Taylor <john.taylor@holidayextras.com>
2020-08-17 13:35:21 +0100

    Non-statis padding

--- components/StatPanel.styles.js
+++ components/StatPanel.styles.js
@@ -1,9 +1,10 @@
 import { StyleSheet } from 'react-native';
+import { padding } from '../styles/base.js';
 
 export default StyleSheet.create({
   body: {
     margin: 6,
-    padding: 10,
+    padding: padding.sm,
     backgroundColor: '#e5e6eb',
     borderRadius: 5,
     flex: 1
